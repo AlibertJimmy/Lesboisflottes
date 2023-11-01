53814af7d65210594a65a3ffbaade09d3a664236
Jimmy Alibert <jimmy.alibert@laposte.net>
2023-11-01 15:22:16 +0100

    Actualized .gitignore

---
+++ src/datas/contact.jsx
@@ -0,0 +1,19 @@
+import julie from '../assets/profiles/julie.png'
+import jm from '../assets/profiles/jm.png'
+
+
+export const contactList = [
+    {   
+        "name": "Julie",
+        "telephone": "061abcd",
+        "email": "email123",
+        "profilePic":julie
+    },
+    {   
+        "name": "J.Manuel",
+        "telephone": "06123",
+        "email": "email123",
+        "profilePic":jm
+
+    }
+]
