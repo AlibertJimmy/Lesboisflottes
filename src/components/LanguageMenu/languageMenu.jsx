43eeb0cb2b57b0772a2a624c39981d5634a0a615
Jimmy Alibert <jimmy.alibert@laposte.net>
2023-11-03 18:57:38 +0100

    Reworked MenuLanguage to display flag instead of label

--- src/components/LanguageMenu/languageMenu.jsx
+++ src/components/LanguageMenu/languageMenu.jsx
@@ -1,8 +1,60 @@
+// Import React Libraries
+import React from "react";
+import Select from "react-select";
 import { useTranslation } from "react-i18next";
 
+// Import Datas
 import { LANGUAGES } from "../../constants/language";
 
+// Import Style
+import styled from "styled-components";
 
+const StyledSelect = styled.div`
+  width: 100px;
+  position: fixed;
+  right: 8px; ;
+`
+
+export const LanguageMenu = () => {
+  const { i18n } = useTranslation();
+
+  const onChangeLang = (selectedOption) => {
+    const lang_code = selectedOption.value;
+    i18n.changeLanguage(lang_code);
+  };
+
+  // Create an array of custom options with images
+  const customOptions = LANGUAGES.map(({ code, label, flag }) => ({
+    value: code,
+    label: (
+      <div style={{display:'flex', alignItems:'center', justifyContent:'space-between'}}>
+        <img src={flag} alt={label} style={{ height: '20px', width: '40px' }} /><span>{label}</span>
+      </div>
+    ),
+  }));
+
+  const customSelectRender = LANGUAGES.map(({ code, label, flag }) => ({
+    value: code,
+    label: (
+      <div style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
+        <img src={flag} alt={label} style={{ height: '20px', width: '40px' }} />
+      </div>
+    ),
+  }));
+
+  return (
+    <StyledSelect>
+      <Select
+        options={customOptions}
+        defaultValue={customSelectRender.find((option) => option.value === i18n.language)}
+        onChange={onChangeLang}
+      />
+    </StyledSelect>
+  );
+};
+
+
+/*
 export const LanguageMenu = () => {
   const { i18n } = useTranslation();
 
@@ -24,3 +76,4 @@ export const LanguageMenu = () => {
     </nav>
   );
 };
+*/
--- src/constants/language.jsx
+++ src/constants/language.jsx
@@ -1,7 +1,17 @@
-
+import flag_fr from '../assets/flag/flag_fr.png'
+import flag_en from '../assets/flag/flag_en.png'
 
 export const LANGUAGES = [
-    { label: "Français", code: "fr" },
-    { label: "English", code: "en" },
+    { 
+      label: "FR", 
+      code: "fr",
+      flag: flag_fr
+    },
+    { 
+      label: "EN", 
+      code: "en",
+      flag: flag_en
+    },
   ];
   
+
