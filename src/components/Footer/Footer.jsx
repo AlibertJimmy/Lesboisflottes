fc6da9f54a5ad8a7732e215da0cf610f06caf14e
Jimmy Alibert <jimmy.alibert@laposte.net>
2024-01-12 11:24:52 +0100

    Improved footer copryrights

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 // Import React Libraries
 import React from 'react';
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { useTranslation } from 'react-i18next';
+import { format } from 'date-fns';
 
 // Import Functions
 import { scrollToTop } from '../../utils/functions/General';
@@ -14,11 +16,13 @@ import {
 import { StyledIcon } from '../../utils/style/js/IconStyle';
 
 function Footer () {
+  const { t } = useTranslation();
+  const currentYear = format(new Date(), 'yyyy');
   return (
     <StyledFooter>
         <FooterSideContainerLeft></FooterSideContainerLeft>
         <FooterTextContainer id='footerTextContainer'>
-            <StyledP>Copyright © 2023 Les Bois Flottés</StyledP>
+            <StyledP>Copyright © {currentYear} {t('AllRightsReserved')}</StyledP>
         </FooterTextContainer>
         <FooterSideContainerRight onClick={() => scrollToTop()}>
             <StyledIcon id="scrollToTopChevron" icon={faChevronUp} />
