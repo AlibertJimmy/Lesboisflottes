8d61eaf047e41fb5592763bd3e04b14ac4603b6c
Jimmy Alibert <jimmy.alibert@laposte.net>
2024-04-18 14:37:04 +0200

    Corrected Dvp Url

--- src/utils/constants/Language_Constants.js
+++ src/utils/constants/Language_Constants.js
@@ -16,7 +16,7 @@ export const LANGUAGES = [
 ];
 
 export const webSiteDevUrl = 'http://localhost:3000';
-export const webSiteProdUrl = 'development.d1jcxno1msc9kd.amplifyapp.com';
+export const webSiteProdUrl = 'https://development.d1jcxno1msc9kd.amplifyapp.com';
 
 // Function to detect if it's a langage code
 export const isLanguageCode = (codeToCheck) => {
