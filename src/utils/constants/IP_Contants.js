5c0435122aa9e5bb7543d34c9dd9a4b03afaaefc
Jimmy Alibert <jimmy.alibert@laposte.net>
2024-05-13 10:39:34 +0200

    returned to api request using dns instead of ip address

--- src/utils/constants/IP_Contants.js
+++ src/utils/constants/IP_Contants.js
@@ -1,3 +1,3 @@
-// export const WebServerIPAddress = 'alttechweb-server.com';
-export const WebServerIPAddress = '13.49.95.163';
+export const WebServerIPAddress = 'alttechweb-server.com';
+
 export const WebServerContactPort = 3000;
